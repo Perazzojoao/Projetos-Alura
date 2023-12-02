fcb29d9353f93ba5f1a7659ec35dd8212c5de8c0
Perazzojoao <perazzojoao@gmail.com>
2023-12-02 16:33:29 -0300

    Projeto final

--- React.js/Organo/src/componentes/Colaborador/index.js
+++ React.js/Organo/src/componentes/Colaborador/index.js
@@ -1,7 +1,14 @@
 import { TiDelete } from "react-icons/ti";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+
+    const propsFavorito = {                              // --> Passando props como obj
+        size: 25,
+        onClick: () => {aoFavoritar(colaborador.id)}
+    }
+
     return (<div className="colaborador">
         <TiDelete 
             size={25} 
@@ -14,6 +21,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito 
+                    ? <MdFavorite {...propsFavorito} color='#ff0000'/> 
+                    : <MdFavoriteBorder {...propsFavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
--- React.js/Organo/src/componentes/Time/index.js
+++ React.js/Organo/src/componentes/Time/index.js
@@ -2,14 +2,22 @@ import hexToRgba from 'hex-to-rgba'
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.5') }}>
             <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color'className='input-color'/>
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/>;
+                    return (
+                        <Colaborador 
+                            key={indice} 
+                            colaborador={colaborador} 
+                            corDeFundo={time.cor} 
+                            aoDeletar={aoDeletar} 
+                            aoFavoritar={aoFavoritar} 
+                        />
+                    );
                 })}
             </div>
         </section>
