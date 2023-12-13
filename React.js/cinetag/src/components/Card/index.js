eb9403a536877e4b34dd0d468cd98d2868182181
Perazzojoao <perazzojoao@gmail.com>
2023-12-13 13:25:34 -0300

    Adição de addFavorito e lista de favoritos

--- React.js/cinetag/src/components/Card/index.js
+++ React.js/cinetag/src/components/Card/index.js
@@ -1,13 +1,30 @@
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
 import iconeDesfavoritar from './desfavoritar.png';
+import { useFavoritoContext } from 'components/Context/Favoritos';
+
+const verificaFavorito = (favorito, id) => {
+	const favAtual = favorito.find(video => video.id === id);
+
+	if (favAtual) {
+		return iconeDesfavoritar;
+	}
+	return iconeFavoritar;
+}
 
 const Card = ({ id, titulo, capa }) => {
+	const { favorito, addFavorito } = useFavoritoContext();
+
 	return (
 		<div className={styles.container}>
 			<img className={styles.capa} src={capa} alt='Capa do vídeo' />
 			<h2>{titulo}</h2>
-			<img className={styles.favoritar} src={iconeFavoritar} alt='Imagem de um coração' />
+			<img
+				className={styles.favoritar}
+				src={verificaFavorito(favorito, id)}
+				alt='Imagem de um coração'
+				onClick={() => addFavorito({ id, titulo, capa })}
+			/>
 		</div>
 	);
 };
--- React.js/cinetag/src/pages/Favoritos/index.js
+++ React.js/cinetag/src/pages/Favoritos/index.js
@@ -1,18 +1,29 @@
 import Card from 'components/Card';
 import styles from './Favoritos.module.css';
-import Titulo from "components/Titulo";
+import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'components/Context/Favoritos';
 
 const Favoritos = () => {
-  return ( 
-    <main>
-      <Titulo>
-        <h1>Meus Favoritos</h1>
-      </Titulo>
-      <section className={styles.container} >
-        <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
-      </section>
-    </main>
-  );
-}
- 
+	const { favorito, addFavorito } = useFavoritoContext();
+
+	return (
+		<main>
+			<Titulo>
+				<h1>Meus Favoritos</h1>
+			</Titulo>
+			<section className={styles.container}>
+				{favorito.map((video) => (
+					<Card
+						key={video.id}
+						id={video.id}
+						titulo={video.titulo}
+						capa={video.capa}
+						video={video}
+					/>
+				))}
+			</section>
+		</main>
+	);
+};
+
 export default Favoritos;
