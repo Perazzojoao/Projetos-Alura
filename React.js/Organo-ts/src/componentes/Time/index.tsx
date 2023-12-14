d066100d9bf797b38a857048a2db1f1a28db0682
Perazzojoao <perazzojoao@gmail.com>
2023-12-14 02:18:04 -0300

    adição de tipagem de Time

---
+++ React.js/Organo-ts/src/componentes/Time/index.tsx
@@ -0,0 +1,41 @@
+import Colaborador from '../Colaborador';
+import './Time.css';
+
+interface TimeProps {
+	corSecundaria: string;
+	corPrimaria: string;
+	nome: string;
+	colaboradores: [
+		{
+			nome: string;
+			cargo: string;
+			imagem: string;
+			time: string;
+		}
+	];
+}
+
+const Time = ({ nome, corPrimaria, corSecundaria, colaboradores }: TimeProps) => {
+	const css = { backgroundColor: corSecundaria };
+
+	return colaboradores.length > 0 ? (
+		<section className='time' style={css}>
+			<h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+			<div className='colaboradores'>
+				{colaboradores.map((colaborador) => (
+					<Colaborador
+						corDeFundo={corPrimaria}
+						key={colaborador.nome}
+						nome={colaborador.nome}
+						cargo={colaborador.cargo}
+						imagem={colaborador.imagem}
+					/>
+				))}
+			</div>
+		</section>
+	) : (
+		''
+	);
+};
+
+export default Time;
