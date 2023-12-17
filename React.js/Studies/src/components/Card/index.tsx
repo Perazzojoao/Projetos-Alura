b3e5593a1b32b2f317a5000d9e3ffe6457bb051e
Perazzojoao <perazzojoao@gmail.com>
2023-12-17 01:06:31 -0300

    Adição de seleção de cards final

--- React.js/Studies/src/components/Card/index.tsx
+++ React.js/Studies/src/components/Card/index.tsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IFormValues } from '../../interfaces/IFormValues';
-import styles from './Card.module.css';
 import { useIsActive } from '../contexts/IsActive';
+import styles from './Card.module.css';
 
 const Card = ({ tarefa, tempo }: IFormValues) => {
 	const [ativo, setAtivo] = useState(false);
 	const { isActive, addIsActive } = useIsActive();
 
-	const activate = () => {
-		addIsActive(tarefa);
-		setAtivo(false);
-		if (isActive === tarefa) {
+	useEffect(() => {
+		if (isActive.tarefa !== tarefa) {
+			setAtivo(false);
+		} else {
 			setAtivo(true);
 		}
-	}
+		
+	}, [isActive]);
 
 	return (
 		<div
 			className={`${styles.container} ${ativo ? styles.containerActive : ''}`}
-			onClick={activate}
+			onClick={() => addIsActive({ tarefa, tempo })}
     >
 			<label>{tarefa}</label>
 			<span>{tempo}</span>
--- React.js/Studies/src/components/contexts/IsActive.tsx
+++ React.js/Studies/src/components/contexts/IsActive.tsx
@@ -1,20 +1,21 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import { IFormValues } from '../../interfaces/IFormValues';
 
 interface IsActiveProvider {
   children: ReactNode;
 }
 
 interface IContext {
-  isActive: string;
-  setIsActive: (value: string | (CallBack)) => void;
+  isActive: IFormValues;
+  setIsActive: (value: IFormValues | (CallBack)) => void;
 }
 
-type CallBack = (prev: string) => string;
+type CallBack = (prev: IFormValues) => IFormValues;
 
 const IsActiveContext = createContext({} as IContext);
 
 const IsActiveProvider = ({ children }: IsActiveProvider) => {
-  const [ isActive, setIsActive ] = useState<string>('');
+  const [ isActive, setIsActive ] = useState<IFormValues>({} as IFormValues);
   return ( 
     <IsActiveContext.Provider value={{isActive, setIsActive}}>
       {children}
@@ -25,18 +26,17 @@ const IsActiveProvider = ({ children }: IsActiveProvider) => {
 export function useIsActive () {
   const { isActive, setIsActive } = useContext(IsActiveContext);
 
-  function addIsActive (tarefa: string) {
-    if (isActive === '') {
-      setIsActive(tarefa);
-    } else {
-      setIsActive(prev => {
-        if (prev === tarefa) {
-          return '';
-        }
-        return tarefa;
-      })
-    }
-    console.log(tarefa);
+  function addIsActive ({ tarefa, tempo }: IFormValues) {
+    // const inicialValue = {tarefa: '', tempo: ''}
+    // setIsActive({tarefa: '', tempo: ''});
+    
+    setIsActive(prev => {
+      if (prev.tarefa === tarefa) {
+        return {tarefa: '', tempo: ''};
+      }
+      return { tarefa, tempo };
+    })
+    console.log(isActive);
   }
 
   return { isActive, addIsActive };
