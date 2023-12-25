8f5dc99ca432257027cca465c5ac34f4a0a8fa5e
Perazzojoao <perazzojoao@gmail.com>
2023-12-25 00:23:56 -0300

    Criação de arquivo eslint

---
+++ React.js/TypeScript/Aluroni-router/src/pages/Inicio/index.tsx
@@ -0,0 +1,14 @@
+import cardapio from 'pages/Cardapio/Itens/itens.json';
+
+const Inicio = () => {
+  return (
+    <section>
+      <h3>Recomendações da cozinha</h3>
+      <div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Inicio;
