f67187a744c5a948f7d83eba9a64e84d00b34bc8
Perazzojoao <perazzojoao@gmail.com>
2023-12-28 19:46:48 -0300

    Adição de listagem de restaurantes pela API

--- React.js/TypeScript/Alfood/src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ React.js/TypeScript/Alfood/src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import axios from 'axios';
+import IPrato from '../../../interfaces/IPrato';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>([]);
+
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(response => {
+        setPratos(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [restaurante.id]);
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
