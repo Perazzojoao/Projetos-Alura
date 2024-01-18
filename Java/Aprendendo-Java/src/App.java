3355c12da07c40bf7c618a84e92ec52128f9370f
Perazzojoao <perazzojoao@gmail.com>
2024-01-17 21:58:51 -0300

    Att

--- Java/Aprendendo-Java/src/App.java
+++ Java/Aprendendo-Java/src/App.java
@@ -1,5 +1,24 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme: Top Gun: Maverick");
+
+    int ano = 2022;
+
+    System.out.println("Ano de lançamento: " + ano);
+
+    String sinopse = "Sinopse Top Gun:";
+
+    String textoFormatado = """
+
+        Sinopse do filme
+        Top Gun:
+
+        Filme de aventura com Tom Cruize.
+        Muito bom!
+        ...
+        """;
+
+    System.out.println(sinopse + textoFormatado);
+  }
 }
