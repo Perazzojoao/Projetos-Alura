0ca726452155f5e5a5ecf8dbf3196240d63fd7a3
Perazzojoao <perazzojoao@gmail.com>
2024-01-19 22:38:21 -0300

    Adição de novo projeto

---
+++ Java/Operacoes-Bancarias/src/App.java
@@ -0,0 +1,12 @@
+import dadosCliente.Cliente;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Cliente cliente = new Cliente();
+
+        cliente.InserirDados();
+
+        System.out.println();
+        
+    }
+}
