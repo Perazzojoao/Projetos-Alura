35da5f1529f7febc52bfba9bbc88e6be5c616263
Perazzojoao <perazzojoao@gmail.com>
2024-01-25 23:20:32 -0300

    att

--- React.js/TypeScript/Newsletter/src/components/Header/index.tsx
+++ React.js/TypeScript/Newsletter/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import ToggleTheme from "../ToggleTheme";
 
 const Header = () => {
 	return (
-		<div className='flex h-20 justify-between items-center bg-gray-500 px-5 font-bold iPhone:rounded-xl iPhone:m-5'>
+		<div className='flex h-20 justify-between items-center bg-alura-200 dark:bg-dark-200 px-5 font-bold iPhone:rounded-xl iPhone:m-5'>
 			<span className='text-gray-200'>Olá, usuário</span>
 			<h1 className='text-gray-200 text-xl'>Alura NewsLetter</h1>
       <ToggleTheme />      
--- React.js/TypeScript/Newsletter/tailwind.config.js
+++ React.js/TypeScript/Newsletter/tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        'iPhone': '375px'
-      },
-    },
-  },
-  plugins: [],
-}
-
+	darkMode: 'class',
+	content: ['./index.html', './src/**/*.{js,ts,tsx}'],
+	theme: {
+		extend: {
+			screens: {
+				iPhone: '375px',
+			},
+			colors: {
+				dark: {
+					100: 'rgb(18, 18, 18)',
+					200: 'rgb(51, 51, 51)',
+				},
+				alura: {
+					100: '#167BF7',
+					200: '#051933',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
