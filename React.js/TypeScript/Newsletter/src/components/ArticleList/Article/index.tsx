f7aeac174a381d089f145f68bf0f74fb516d8e9e
Perazzojoao <perazzojoao@gmail.com>
2024-01-26 19:23:35 -0300

    Att

--- React.js/TypeScript/Newsletter/src/components/ArticleList/Article/index.tsx
+++ React.js/TypeScript/Newsletter/src/components/ArticleList/Article/index.tsx
@@ -2,14 +2,14 @@ type ArticleProps = {
 	title: string;
 	text: string[];
 	tags: string[];
-  image?: string;
-  alt?: string;
+	image?: string;
+	alt?: string;
 };
 
 const Article = ({ title, text }: ArticleProps) => {
 	return (
 		<div className='p-5 bg-gray-300 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg'>
-			<h2 className='text-xl font-bold mb-2'>{title}</h2>
+			<h2 className='text-xl font-bold text-alura-200 dark:text-gray-200 mb-2'>{title}</h2>
 			<div className='grid gap-1'>
 				{text.map((paragrafo, i) => (
 					<p key={i} className='text-justify text-alura-200 dark:text-gray-300'>
