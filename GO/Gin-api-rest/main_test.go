7db52ebbc46233e7d78b8b70411fff294a635e32
Perazzojoao <perazzojoao@gmail.com>
2024-02-23 14:13:27 -0300

    Adiciona testes de unidade para adicionar aluno

--- GO/Gin-api-rest/main_test.go
+++ GO/Gin-api-rest/main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +14,10 @@ import (
 
 	"api-go-gin/controllers"
 	"api-go-gin/database"
+	"api-go-gin/models"
 )
+
+var ID int
 
 func SetupRoutersTest() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
@@ -19,7 +25,24 @@ func SetupRoutersTest() *gin.Engine {
 	return r
 }
 
-func TestRouteExemplo(t *testing.T) {
+func AddAlunoTeste() {
+	database.ConectarDB()
+	var a = models.Aluno{CPF: "01234567890", Nome: "Teste", RG: "123456789"}
+	database.DB.Create(&a)
+	ID = int(a.ID)
+}
+
+func DeleteAluno(t *testing.T) {
+	if ID == 0 {
+		t.Fatal("ID = 0, não é possível deletar aluno.")
+	}
+
+	database.ConectarDB()
+	var a models.Aluno
+	database.DB.Delete(&a, fmt.Sprint(ID))
+}
+
+func TestExemplo(t *testing.T) {
 	r := SetupRoutersTest()
 	r.GET("/alunos/exemplo", controllers.Exemplo)
 
@@ -49,4 +72,27 @@ func TestGetAllAlunos(t *testing.T) {
 
 	respBody, _ := io.ReadAll(resp.Body)
 	assert.NotEmpty(string(respBody))
+}
+
+func TestAddAluno(t *testing.T) {
+	database.ConectarDB()
+	r := SetupRoutersTest()
+	r.POST("/alunos", controllers.AddAluno)
+
+	mockResp := `{"cpf":"01234567890","nome":"Teste","rg":"123456789"}`
+	req, _ := http.NewRequest("POST", "/alunos", bytes.NewReader([]byte(mockResp)))
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert := assert.New(t)
+	assert.Equal(http.StatusOK, resp.Code)
+
+	respBody, _ := io.ReadAll(resp.Body)
+	assert.NotEmpty(string(respBody))
+
+	var a models.Aluno
+	json.Unmarshal(respBody, &a)
+	ID = int(a.ID)
+
+	DeleteAluno(t)
 }
