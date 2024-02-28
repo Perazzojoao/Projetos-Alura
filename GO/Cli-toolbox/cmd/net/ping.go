05a3cc4187d04857b4e914c6f2b0d62a962e3ee6
Perazzojoao <perazzojoao@gmail.com>
2024-02-28 16:14:15 -0300

    Adiciona comando "ping" para enviar um ping para uma URL remota e retornar a resposta.

--- GO/Cli-toolbox/cmd/net/ping.go
+++ GO/Cli-toolbox/cmd/net/ping.go
@@ -5,21 +5,47 @@ package net
 
 import (
 	"fmt"
+	"net"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var urlPath string
+var cliente = http.Client{
+	Transport: &http.Transport{
+		Dial: (&net.Dialer{Timeout: 3 * time.Second}).Dial,
+	},
+}
+
+func ping(domain string) (int, error) {
+	url := "https://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := cliente.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+}
 
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
 	Short: "Send a ping to a URL.",
-	Long: `Send a ping to a URL. For example: 
+	Long: `Send a ping to a remote URL ande returns the response. For example: 
 
 toolbox net ping -u https://www.google.com`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println("StatusCode", resp, "OK")
+		}
 	},
 }
 
