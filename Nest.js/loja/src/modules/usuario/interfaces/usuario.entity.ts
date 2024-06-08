e06b10428edec809a273c93721727d4412b6808a
Perazzojoao <perazzojoao@gmail.com>
2024-06-08 13:54:54 -0300

    Refatoração de código

--- Nest.js/loja/src/modules/usuario/interfaces/usuario.entity.ts
+++ Nest.js/loja/src/modules/usuario/interfaces/usuario.entity.ts
@@ -1,11 +1,8 @@
-import { PrismaService } from 'src/database/prisma.service';
-
 export class UsuarioEntity {
   id: number;
   nome: string;
   email: string;
   senha: string;
-  [key: string]: any;
 
   constructor(nome: string, email: string, senha: string) {
     this.nome = nome;
